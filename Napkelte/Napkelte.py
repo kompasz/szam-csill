ed5dbd2b1d37c5c374d0a779e2941c41cded7659
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-04-21 20:05:46 +0300

    ekl, equ

--- Napkelte/Napkelte.py
+++ Napkelte/Napkelte.py
@@ -1,22 +1,55 @@
-
+import math
 
 class Napkelte:
     def __init__(self):
-
-        pass
-
-    def Nap_ekl(self, be_JD):
         
         pass
 
+    def Nap_ekl(self, be_JD):
+        T = (be_JD - 2451545) / 36525
+        M = 0.993133 + 99.997361 * T
+        Mfrac = M - math.floor(M)
+        Mrad = 2 * math.pi * Mfrac
+
+        L = 0.7859453 + Mrad / (2 * math.pi) + (6893 * math.sin(Mrad) + 72*math.sin(2 * Mrad) + 6191*T) / 1296000
+        Lfrac = L - math.floor(L)
+        Lrad = 2 * math.pi * Lfrac
+        Lfok = Lrad * 180 / math.pi
+        Bfok = 0
+
+        return Lfok, Bfok
+        
+    # Nap_equ(2458942.875)=0.863906287503828 5.552314928465703
     def Nap_equ(self, be_JD):
+        # ekliptikai koordinatak
+        # λ - alfa
+        # β - beta
+        # egyenlitoi koordinatak
+        # δ - ra
+        # α - dec
+        alfa, beta = self.Nap_ekl(be_JD)
+        T = (be_JD - 2451545) / 36525
+        eps = 23.43929111 - T * (46.8150 + T * (0.00059 - 0.001813 * T)) / 3600    
+        eps = math.radians(eps)
+        alfa = math.radians(alfa)
+        beta = math.radians(beta)
+        dec = math.asin(math.sin(eps)*math.cos(beta)*math.sin(alfa) + math.cos(eps)*math.sin(beta))
 
-        pass
+        ra = math.acos(math.cos(alfa)*math.cos(beta) / math.cos(dec))
+        # 1 Radians ... 3.8197 Hour angles
+        # n rad     ...  x    hours
+        return ra * 3.8197, math.degrees(dec)
 
-    def kel_nyugszik(self, be_RA, be_Dec, be_szelesseg, be_magassag)):
+    # kel_nyugszik(4.5,15,45,0)=21.4638 248.5293 11.5362 111.4707
+    def kel_nyugszik(self, be_RA, be_Dec, be_szelesseg, be_magassag):
 
         pass
 
     def KN(self, be_ev, be_honap, be_nap, be_szelesseg, be_hosszusag, be_idozona):
 
         pass
+
+nap = Napkelte()
+# print(nap.Nap_ekl(2458942.7850))
+# print(nap.Nap_ekl(2458942.875))
+print(nap.Nap_equ(2458942.875))
