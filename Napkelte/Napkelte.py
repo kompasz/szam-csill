12f2cf5bdb62fa8ca83f61b86fd57d8e3a967367
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-04-21 16:47:05 +0300

    Napkelte base

---
+++ Napkelte/Napkelte.py
@@ -0,0 +1,22 @@
+
+
+class Napkelte:
+    def __init__(self):
+
+        pass
+
+    def Nap_ekl(self, be_JD):
+        
+        pass
+
+    def Nap_equ(self, be_JD):
+
+        pass
+
+    def kel_nyugszik(self, be_RA, be_Dec, be_szelesseg, be_magassag)):
+
+        pass
+
+    def KN(self, be_ev, be_honap, be_nap, be_szelesseg, be_hosszusag, be_idozona):
+
+        pass
