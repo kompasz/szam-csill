94ad2c39a55b8f39f75ed4136b24adb2c7230e6b
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-04-28 20:41:31 +0300

    newton method

--- Kepler-Egyenletek/Kepler.py
+++ Kepler-Egyenletek/Kepler.py
@@ -16,12 +16,25 @@ class Kepler:
 
         return E
 
+    def szamol_newton_method(self, e, hiba, M):
+
+        E = M
+        Ekov = E - (E - e*math.sin(E) - M) / (1 - e*math.cos(E))
+
+        while (abs(Ekov - E) > hiba): 
+            E = Ekov
+            Ekov = E - (E - e*math.sin(E) - M) / (1 - e*math.cos(E))
+
+        return E
 
 kepler = Kepler()
 
 # M= 0éstetsz®legeseeseténE= 0;
 print(kepler.szamol_iterativ(10, pow(1/10, 14), 0))
+print(kepler.szamol_newton_method(10, pow(1/10, 14), 0))
 # M=πéstetsz®legeseeseténE=π,azaztizenkéttizedesrekerekítve3.141592653590;
 print(kepler.szamol_iterativ(10, pow(1/10, 14), math.pi))
+print(kepler.szamol_newton_method(10, pow(1/10, 14), math.pi))
 # Kepler(0.98,1E-14,3.5)=3.323098922816507.1
 print(kepler.szamol_iterativ(0.98, pow(1/10, 14), 3.5))
+print(kepler.szamol_newton_method(0.98, pow(1/10, 14), 3.5))
