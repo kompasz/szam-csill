d5a15717e65365fb8ff05fea935bc426b9389781
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-04-29 08:13:29 +0300

    Kepler final

--- Kepler-Egyenletek/Kepler.py
+++ Kepler-Egyenletek/Kepler.py
@@ -27,6 +27,15 @@ class Kepler:
 
         return E
 
+    def szamol_mas(self, hiba, M):
+        E = M
+        Ekov = M + math.sin(E) 
+        while (abs(Ekov - E) > hiba):
+            E = Ekov
+            Ekov = M + math.sin(E)   
+            print(E)
+        return E
+
 kepler = Kepler()
 
 # M= 0éstetsz®legeseeseténE= 0;
@@ -38,3 +47,12 @@ print(kepler.szamol_newton_method(10, pow(1/10, 14), math.pi))
 # Kepler(0.98,1E-14,3.5)=3.323098922816507.1
 print(kepler.szamol_iterativ(0.98, pow(1/10, 14), 3.5))
 print(kepler.szamol_newton_method(0.98, pow(1/10, 14), 3.5))
+
+# print(kepler.szamol_iterativ(0.01, pow(1/10, 10), 2))
+# print(kepler.szamol_iterativ(0.1, pow(1/10, 10), 1))
+# print(kepler.szamol_iterativ(0.99, pow(1/10, 14), 2))
+# print(kepler.szamol_iterativ(0.1, pow(1/10, 14), 1))
+# print(kepler.szamol_mas(pow(1/10, 14), 2))
+# print(kepler.szamol_iterativ(0.002, pow(1/10, 14), 0))
+# print(math.degrees(kepler.szamol_iterativ(0.02, pow(1/10, 14), math.radians(2))))
+print(kepler.szamol_iterativ(0.99, pow(1/10, 14), math.radians(2)))
