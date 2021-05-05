98bf63e55cd0534d89cfdede7a825c9ec5eb586e
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-05-05 18:52:04 +0300

    Bolygo ekl

--- Bolygok/Bolygok.py
+++ Bolygok/Bolygok.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import numpy as np
+import math
 
 class Bolygok:
 
@@ -21,49 +22,83 @@ class Bolygok:
         pass
 
     def Palyaelemek(self, n, julian_evszazadok):
-        Merkur_2000     =[ 0.38709893,   0.20563069,  7.00487,  48.33167,  77.45645,  252.25084]
-        Venusz_2000     =[ 0.72333199,   0.00677323,  3.39471,  76.68069,  131.53298,  181.97973]
-        Fold_2000       =[ 1.00000011,   0.01671022,  0.00005,  -11.26064,  102.94719,  100.46435]
-        Mars_2000       =[ 1.52366231,   0.09341233,  1.85061,  49.57854,  336.04084,  355.45332]
-        Jupiter_2000    =[ 5.20336301,   0.04839266,  1.30530,  100.55615,  14.75385,  34.40438]  
-        Szaturnusz_2000 =[ 9.53707032,   0.05415060,  2.48446,  113.71504,  92.43194,  49.94432]  
-        Uranusz_2000    =[ 19.19126393,    0.04716771,   0.76986,   74.22988,   170.96424,   313.23218]   
-        Neptunusz_2000  =[ 30.06896348,  0.00858587, 1.76917, 131.72169, 44.97135, 304.88003]
+        Merkur_2000     = [ 0.38709927,      0.20563593,      7.00497902,      48.33076593,     252.25032350,     77.45779628     ]
+        Venusz_2000     = [ 0.72333566,      0.00677672,      3.39467605,      76.67984255,     181.97909950,    131.60246718     ]
+        Fold_2000       = [ 1.00000261,      0.01671123,     -0.00001531,      0.0,              100.46457166,    102.93768193    ]
+        Mars_2000       = [ 1.52371034,      0.09339410,      1.84969142,      49.55953891,      -4.55343205,    -23.94362959     ]
+        Jupiter_2000    = [ 5.20288700,      0.04838624,      1.30439695,      100.47390909,      34.39644051,     14.72847983    ]  
+        Szaturnusz_2000 = [ 9.53667594,      0.05386179,      2.48599187,      113.66242448,      49.95424423,     92.59887831    ]  
+        Uranusz_2000    = [ 19.18916464,      0.04725744,      0.77263783,    74.01692503,      313.23810451,    170.95427630     ]   
+        Neptunusz_2000  = [ 30.06992276,      0.00859048,      1.77004347,    131.78422574,      -55.12002969,     44.96476227    ]
+        Pluto           = [ 39.48211675,      0.24882730,     17.14001206,    110.30393684,      238.92903833,    224.06891629    ]
 
-        Merkur_kul = [ 0.00000066, 0.00002527, -23.51, -446.30,  573.57, 538101628.29]
-        Venusz_kul = [ 0.00000092, -0.00004938, -2.86, -996.89,  -108.80, 210664136.06]
-        Fold_kul = [ -0.00000005, -0.00003804, -46.94, -18228.25,  1198.28, 129597740.63]
-        Mars_kul = [ -0.00007221, 0.00011902, -25.47, -1020.19,  1560.78, 68905103.78]
-        Jupiter_kul = [ 0.00060737, -0.00012880, -4.15, 1217.17,  839.93, 10925078.35]
-        Szaturnusz_kul = [ -0.00301530, -0.00036762, 6.11, -1591.05,  -1948.89, 4401052.95]
-        Uranusz_kul = [ 0.00152025, -0.00019150, -2.09, -1681.40,  1312.56, 1542547.79]
-        Neptunusz_kul = [  -0.00125196,  0.0000251,  -3.64,  -151.25,   -844.43,  786449.21]
+        # a
+        # e 
+        # I 
+        # omega
+        # w
+        # L
 
+        Merkur_kul       = [ 0.00000037,      0.00001906,     -0.00594749,  -0.12534081,        149472.67411175,      0.16047689  ]
+        Venusz_kul       = [ 0.00000390,     -0.00004107,     -0.00078890,  -0.27769418,        58517.81538729,      0.00268329   ]
+        Fold_kul         = [ 0.00000562,     -0.00004392,     -0.01294668,   0.0,               35999.37244981,      0.32327364   ]
+        Mars_kul         = [ 0.00001847,      0.00007882,     -0.00813131,  -0.29257343,        19140.30268499,      0.44441088   ]
+        Jupiter_kul      = [ -0.00011607,     -0.00013253,     -0.00183714,  0.20469106,        3034.74612775,      0.21252668    ]
+        Szaturnusz_kul   = [ -0.00125060,     -0.00050991,      0.00193609, -0.28867794,        1222.49362201,     -0.41897216    ]
+        Uranusz_kul      = [ -0.00196176,     -0.00004397,     -0.00242939,  0.04240589,         428.48202785,      0.40805281    ]
+        Neptunusz_kul    = [  0.00026291,      0.00005105,      0.00035372, -0.00508664,         218.45945325,     -0.32241464    ]
+        Pluto_kul        = [ -0.00031596,      0.00005170,      0.00004818, -0.01183482,         145.20780515,     -0.04062942    ]
+
+        x = []
         if n == 1:
-            return np.add(Merkur_2000 , julian_evszazadok * Merkur_kul if len(julian_evszazadok * Merkur_kul) != 0 else np.empty(6) )
+            x = np.add(Merkur_2000 , julian_evszazadok * Merkur_kul if len(julian_evszazadok * Merkur_kul) != 0 else np.empty(6) )
         elif n == 2:
-            return np.add(Venusz_2000 , julian_evszazadok * Venusz_kul if len(julian_evszazadok * Venusz_kul) != 0 else np.empty(6))
+            x = np.add(Venusz_2000 , julian_evszazadok * Venusz_kul if len(julian_evszazadok * Venusz_kul) != 0 else np.empty(6))
         elif n == 3:
-            return np.add(Fold_2000 , julian_evszazadok * Fold_kul if len(julian_evszazadok * Fold_kul) != 0 else np.empty(6))
+            x = np.add(Fold_2000 , julian_evszazadok * Fold_kul if len(julian_evszazadok * Fold_kul) != 0 else np.empty(6))
         elif n == 4:
-            return np.add(Mars_2000 , julian_evszazadok * Mars_kul if len(julian_evszazadok * Mars_kul) != 0 else np.empty(6))
+            x = np.add(Mars_2000 , julian_evszazadok * Mars_kul if len(julian_evszazadok * Mars_kul) != 0 else np.empty(6))
         elif n == 5:
-            return np.add(Jupiter_2000 , julian_evszazadok * Jupiter_kul if len(julian_evszazadok * Jupiter_kul) != 0 else np.empty(6))
+            x = np.add(Jupiter_2000 , julian_evszazadok * Jupiter_kul if len(julian_evszazadok * Jupiter_kul) != 0 else np.empty(6))
         elif n == 6:
-            return np.add(Szaturnusz_2000 , julian_evszazadok * Szaturnusz_kul if len(julian_evszazadok * Szaturnusz_kul) != 0 else np.empty(6))
+            x = np.add(Szaturnusz_2000 , julian_evszazadok * Szaturnusz_kul if len(julian_evszazadok * Szaturnusz_kul) != 0 else np.empty(6))
         elif n == 7:
-            return np.add(Uranusz_2000 , julian_evszazadok * Uranusz_kul if len(julian_evszazadok * Uranusz_kul) != 0 else np.empty(6))
+            x = np.add(Uranusz_2000 , julian_evszazadok * Uranusz_kul if len(julian_evszazadok * Uranusz_kul) != 0 else np.empty(6))
         elif n == 8:
-            return np.add(Neptunusz_2000 , julian_evszazadok * Neptunusz_kul if len(julian_evszazadok * Neptunusz_kul) != 0 else np.empty(6))
+            x = np.add(Neptunusz_2000 , julian_evszazadok * Neptunusz_kul if len(julian_evszazadok * Neptunusz_kul) != 0 else np.empty(6))
         elif n == 9:
-            return 'Pluto not a planet.'
+            x = np.add(Pluto_2000 , julian_evszazadok * Pluto_kul if len(julian_evszazadok * Pluto_kul) != 0 else np.empty(6))
         else: 
             raise 'Not supported'
-        pass
+        
+        ered = x[:4]
+        w = x[5]-x[3]
+        ered = np.append(ered, w)
+        M = x[4] - x[5]
+        ered = np.append(ered, M)        
+        return ered
 
-    def Bolygo_ekl(self, bolygo_sorszam, julian_evszazadok):
+    def szamol_iterativ(self, e, hiba, M):
+        E = M
+        Ekov = M + e*math.sin(E) 
+        while (abs(Ekov - E) > hiba):
+            E = Ekov
+            Ekov = M + e*math.sin(E)   
 
-        pass
+        return E
+
+    def Bolygo_ekl(self, n, julian_evszazadok):
+
+        x = self.Palyaelemek(n, julian_evszazadok)
+        E = self.szamol_iterativ(x[1], pow(1/10, 14), x[5])
+
+        x_ = x[0] * (math.cos(E) - x[1])
+        y_ = x[0] * math.sqrt(1 - pow(x[1], 2)) * math.sin(E)
+        z_ = 0
+
+    # TO DO 
+    #  Rotate matrix 
+
 
 bolygok  = Bolygok()
 # n= 1, T= 0eseténakívántpályaelemek:
@@ -75,7 +110,7 @@ bolygok  = Bolygok()
 
 # n= 1, T= 1eseténakívántko ordináták:
 # 0.247511514559500   -0.347901498789926  -0.051119438302676
-# print(bolygok.Bolygo_ekl(1, 1))
+print(bolygok.Bolygo_ekl(1, 1))
 # n= 5, T= 0.2eseténakívántko ordináták:
 # 4.715437497290996   -1.635903557871500  -0.0981287957651891
 # print(bolygok.Bolygo_ekl(5, 0.2))
