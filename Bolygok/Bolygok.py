669d2fa02f4f76f8a2b74eba616e1efc70c52235
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-05-05 17:13:56 +0300

    Palyaelemek

--- Bolygok/Bolygok.py
+++ Bolygok/Bolygok.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 class Bolygok:
 
@@ -15,11 +16,49 @@ class Bolygok:
         Pluto = 9
         
     def __init__(self):
+
         
         pass
 
     def Palyaelemek(self, n, julian_evszazadok):
+        Merkur_2000     =[ 0.38709893,   0.20563069,  7.00487,  48.33167,  77.45645,  252.25084]
+        Venusz_2000     =[ 0.72333199,   0.00677323,  3.39471,  76.68069,  131.53298,  181.97973]
+        Fold_2000       =[ 1.00000011,   0.01671022,  0.00005,  -11.26064,  102.94719,  100.46435]
+        Mars_2000       =[ 1.52366231,   0.09341233,  1.85061,  49.57854,  336.04084,  355.45332]
+        Jupiter_2000    =[ 5.20336301,   0.04839266,  1.30530,  100.55615,  14.75385,  34.40438]  
+        Szaturnusz_2000 =[ 9.53707032,   0.05415060,  2.48446,  113.71504,  92.43194,  49.94432]  
+        Uranusz_2000    =[ 19.19126393,    0.04716771,   0.76986,   74.22988,   170.96424,   313.23218]   
+        Neptunusz_2000  =[ 30.06896348,  0.00858587, 1.76917, 131.72169, 44.97135, 304.88003]
 
+        Merkur_kul = [ 0.00000066, 0.00002527, -23.51, -446.30,  573.57, 538101628.29]
+        Venusz_kul = [ 0.00000092, -0.00004938, -2.86, -996.89,  -108.80, 210664136.06]
+        Fold_kul = [ -0.00000005, -0.00003804, -46.94, -18228.25,  1198.28, 129597740.63]
+        Mars_kul = [ -0.00007221, 0.00011902, -25.47, -1020.19,  1560.78, 68905103.78]
+        Jupiter_kul = [ 0.00060737, -0.00012880, -4.15, 1217.17,  839.93, 10925078.35]
+        Szaturnusz_kul = [ -0.00301530, -0.00036762, 6.11, -1591.05,  -1948.89, 4401052.95]
+        Uranusz_kul = [ 0.00152025, -0.00019150, -2.09, -1681.40,  1312.56, 1542547.79]
+        Neptunusz_kul = [  -0.00125196,  0.0000251,  -3.64,  -151.25,   -844.43,  786449.21]
+
+        if n == 1:
+            return np.add(Merkur_2000 , julian_evszazadok * Merkur_kul if len(julian_evszazadok * Merkur_kul) != 0 else np.empty(6) )
+        elif n == 2:
+            return np.add(Venusz_2000 , julian_evszazadok * Venusz_kul if len(julian_evszazadok * Venusz_kul) != 0 else np.empty(6))
+        elif n == 3:
+            return np.add(Fold_2000 , julian_evszazadok * Fold_kul if len(julian_evszazadok * Fold_kul) != 0 else np.empty(6))
+        elif n == 4:
+            return np.add(Mars_2000 , julian_evszazadok * Mars_kul if len(julian_evszazadok * Mars_kul) != 0 else np.empty(6))
+        elif n == 5:
+            return np.add(Jupiter_2000 , julian_evszazadok * Jupiter_kul if len(julian_evszazadok * Jupiter_kul) != 0 else np.empty(6))
+        elif n == 6:
+            return np.add(Szaturnusz_2000 , julian_evszazadok * Szaturnusz_kul if len(julian_evszazadok * Szaturnusz_kul) != 0 else np.empty(6))
+        elif n == 7:
+            return np.add(Uranusz_2000 , julian_evszazadok * Uranusz_kul if len(julian_evszazadok * Uranusz_kul) != 0 else np.empty(6))
+        elif n == 8:
+            return np.add(Neptunusz_2000 , julian_evszazadok * Neptunusz_kul if len(julian_evszazadok * Neptunusz_kul) != 0 else np.empty(6))
+        elif n == 9:
+            return 'Pluto not a planet.'
+        else: 
+            raise 'Not supported'
         pass
 
     def Bolygo_ekl(self, bolygo_sorszam, julian_evszazadok):
